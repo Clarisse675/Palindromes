39422fb882076ddeddbbb9288dd51bed82076d1f
clarisse-blondel <clarisse.blondel@edu.esiee.fr>
2025-09-28 20:45:38 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #### Fonction secondaire
+""" Vérifie si une chaîne de caractères est un palindrome """
 
 
 def ispalindrome(p):
+    """ Retourne True si p est un palindrome, False sinon """
+    p = p.lower()
+    p = p.translate(str.maketrans("éèêëàâîïôùûüç", "eeeeaaiiouuuc"))
+    p = ''.join(c for c in p if c.isalnum())
+    return p == p[::-1]
 
-    # votre code ici
-    
-    return False
 
 #### Fonction principale
 
 
 def main():
-
+    """ Permet de tester la fonction ispalindrome """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
